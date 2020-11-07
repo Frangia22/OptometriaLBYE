95bbc4524b62c53a2e30cf3aeb87de0e806b656e
Frangia22 <giacherofranc22@gmail.com>
2020-11-07 15:59:43 -0300

    Se creo el archivo base,index y login.html

--- optometrialbye/settings.py
+++ optometrialbye/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'appoptometrialbye',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
